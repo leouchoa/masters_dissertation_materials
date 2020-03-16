67c60ce043d108083ce439fb80033adf375fe976
guiludwig <guilhermeludwig@gmail.com>
2020-03-16 14:10:03 -0300

    teste

--- teste.R
+++ teste.R
@@ -36,17 +36,24 @@ require(tidyr)
 gather(sampleM) %>% ggplot(aes(x = key, y = value)) + geom_boxplot()
 gather(sampleT) %>% ggplot(aes(x = key, y = value)) + geom_boxplot() + facet_wrap(~key, scales = "free_y")
 
+# Reproduz erro
 
-
-
-optim(theta0,
-      fn = LLike_biwm,
-      gr = LLike_biwm_reduced_grad,
-      method = "L-BFGS-B",
-      lower = c(0.001, 0.001, 0.001, -min(sqrt(nus[1]*nus[2])/mean(nus), 0.999)),
-      upper = c(Inf, Inf, Inf, min(sqrt(nus[1]*nus[2])/mean(nus), 0.999)),
-      control = list(fnscale = -1, trace = 6),
-      nus = nus,  mu = mu, # We always use Z = vec(\mathbf{Y} - boldsymbol\mu), maybe center it outside function?
-      coords_matrix = coords_matrix,
-      obs_vec = obs_vec)
+nus <- c(0.5, 0.5)
+mu <- c(1.119256, 2.170499)
+theta0 <- c(1.0, 1.0, 2.0, 0.5)
+obs_vec <- log_cd
+M <- optim(theta0,
+           fn = BivMaternEstim:::LLike_biwm,
+           gr = BivMaternEstim:::LLike_biwm_reduced_grad,
+           method = "L-BFGS-B",
+           lower = c(0.001, 0.001, 0.001, -min(sqrt(nus[1]*nus[2])/mean(nus), 0.999)),
+           upper = c(Inf, Inf, Inf, min(sqrt(nus[1]*nus[2])/mean(nus), 0.999)),
+           control = list(fnscale = -1, trace = 6),
+           nus = nus,  mu = mu, # We always use Z = vec(\mathbf{Y} - boldsymbol\mu), maybe center it outside function?
+           coords_matrix = coords,
+           obs_vec = obs_vec)
 # Olhar mvtnorm
+BivMaternEstim:::LLike_biwm(c(0.332101, 0.497864, 4.91074, 0.503153),
+                            nus = nus,  mu = mu, # We always use Z = vec(\mathbf{Y} - boldsymbol\mu), maybe center it outside function?
+                            coords_matrix = coords,
+                            obs_vec = obs_vec)
