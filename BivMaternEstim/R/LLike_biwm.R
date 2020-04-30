5dd7f85a600601da44393749255af284a8eb3af7
Leonardo Uchoa <leonardouchoa.pedreira@gmail.com>
2020-04-30 19:53:33 -0300

    Corrected a minor bug and added outputs for tomorrows meeting

--- BivMaternEstim/R/LLike_biwm.R
+++ BivMaternEstim/R/LLike_biwm.R
@@ -30,19 +30,19 @@ LLike_biwm <- function(theta,
                                          coords_matrix = coords_matrix)
 
   autocov_matrix_inv <- block_inv(autocov_matrix)
-  
+
   Z <- t(apply(obs_matrix, 1, function(x) x - mu))
-  
+
   as.double(
     -0.5 * (
       block_log_det(autocov_matrix) +
-        t(Z[,1]) %*% autocov_matrix_inv[[1]] %*% Z[,1] +
-        2 * t(Z[,1]) %*% autocov_matrix_inv[[2]] %*% Z[,2] +
-        t(Z[,2]) %*% autocov_matrix_inv[[3]] %*% Z[,2] +
+        t(Z[,1]) %*% autocov_matrix_inv[[1]] %*% as.matrix(Z[,1]) +
+        2 * t(Z[,1]) %*% autocov_matrix_inv[[2]] %*% as.matrix(Z[,1]) +
+        t(Z[,2]) %*% autocov_matrix_inv[[3]] %*% as.matrix(Z[,1]) +
         length(obs_matrix) * log(2*pi)
     ))
-  
-  
+
+
   # as.double(
   #   -1/2 *
   #     (
@@ -56,50 +56,50 @@ LLike_biwm <- function(theta,
 
 
 # ---- Sanity Check ----
-# 
+#
 # aux_assembler_inv <- function(matrix_list){
 #   cbind(
 #     rbind(matrix_list[[1]], t(matrix_list[[2]])),
 #           rbind(matrix_list[[2]], matrix_list[[3]])
 #     )
 # }
-# 
+#
 # aux_assembler_autocov <- function(matrix_list_2){
 #   cbind(
 #     rbind(matrix_list_2[[1]], matrix_list_2[[3]]),
 #     rbind(matrix_list_2[[3]], matrix_list_2[[2]])
 #   )
 # }
-# 
+#
 # autocov_matrix_old <- aux_assembler_autocov(autocov_matrix)
-# 
+#
 # all.equal(log(det(autocov_matrix_old)),
 #           block_log_det(autocov_matrix))
-# 
+#
 # S_inv_old <- solve(autocov_matrix_old)
 # S_inv_new <- aux_assembler_inv(autocov_matrix_inv)
-# 
+#
 # all.equal(S_inv_new,S_inv_old) #hmmm
-# 
-# 
+#
+#
 # kernel_new <- t(Z[,1]) %*% autocov_matrix_inv[[1]] %*% Z[,1] +
 #   2 * t(Z[,1]) %*% autocov_matrix_inv[[2]] %*% Z[,2] +
 #   t(Z[,2]) %*% autocov_matrix_inv[[3]] %*% Z[,2]
-# 
+#
 # kernel_old <- crossprod(as.numeric(Z), solve(autocov_matrix_old, as.numeric(Z)))
-# 
+#
 # kernel_test_new <- t(as.numeric(Z)) %*% S_inv_new %*% as.numeric(Z)
-# 
+#
 # log_lik_new_way <- -0.5 * (
 #   block_log_det(autocov_matrix) +
 #     t(Z[,1]) %*% autocov_matrix_inv[[1]] %*% Z[,1] +
 #     2 * t(Z[,1]) %*% autocov_matrix_inv[[2]] %*% Z[,2] +
 #     t(Z[,2]) %*% autocov_matrix_inv[[3]] %*% Z[,2] +
 #     length(obs_matrix) * log(2*pi)
-#   
+#
 # )
-# 
-# log_lik_old_way <- 
+#
+# log_lik_old_way <-
 #   as.double(
 #     -1/2 *
 #       (
@@ -108,9 +108,9 @@ LLike_biwm <- function(theta,
 #           length(obs_matrix) * log(2*pi)
 #       )
 #   )
-# 
-# log_lik_new_inv_old_mahala <- 
-#   
+#
+# log_lik_new_inv_old_mahala <-
+#
 #   as.double(
 #     -1/2 *
 #       (
@@ -119,5 +119,5 @@ LLike_biwm <- function(theta,
 #         +
 #           length(obs_matrix) * log(2*pi)
 #       )
-#     
+#
 #   )
