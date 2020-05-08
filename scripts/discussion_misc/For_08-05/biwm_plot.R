eb2d0b26650944dca9ca7d3ff71ba9cfdfb9a805
Leonardo Uchoa <leonardouchoa.pedreira@gmail.com>
2020-05-08 14:25:44 -0300

    more things to discusss

---
+++ scripts/discussion_misc/For_08-05/biwm_plot.R
@@ -0,0 +1,36 @@
+biwm_plot <- function(distances,
+                      sigma_1,
+                      sigma_2,
+                      a,
+                      rho,
+                      nus = c(0.5,0.5,0.5),
+                      titulo=NULL,
+                      ...){
+  
+  if(length(nus) != 3){
+    stop("length(nus) != 3")
+  }
+  
+  gen_form <- function(nu_i){
+    
+    fields::Matern(d = distances,
+                         alpha = a,
+                         nu = nu_i) 
+  }
+  
+  dts <- data.frame(
+    distances,
+    C_11 = sigma_1*gen_form(nus[1]),
+    C_12 = rho*sqrt(sigma_1)*sqrt(sigma_2)*gen_form(nus[3]),
+    C_21 = rho*sqrt(sigma_1)*sqrt(sigma_2)*gen_form(nus[3]),
+    C_22 = sigma_2*gen_form(nus[2])) %>%
+    bind_rows(mutate(.,distances = -distances))
+  
+  dts %>% 
+    gather("key","val",-distances) %>% 
+    ggplot(aes(distances,val)) +
+    geom_line() +
+    facet_wrap(~key, scales = "free_y") +
+    labs(y = "", title = titulo, x= "Distancia")
+  
+}
