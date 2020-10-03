d0a5a686e9ecb7fd7f15e5af6c4249f9c015df37
Leonardo Uchoa <leonardouchoa.pedreira@gmail.com>
2020-10-03 07:09:08 -0300

    adding some plots

---
+++ scripts/image_gen_scripts/theta_hist.R
@@ -0,0 +1,21 @@
+library(BivMaternEstim)
+
+library(ggplot2)
+
+true_theta <- c(1,1,2,0.5)
+initial_pts <- BivMaternEstim:::gen_initial_points(true_theta)
+
+theta_hat_hist_res_plot <- theta_hat_hist(
+  true_theta,
+  initial_pts,
+  n_points = 80,
+  nus_vec = c(0.5, 0.5),
+  n_replicates = 500,
+  seed_number = sample.int(12312312, 1)
+)
+
+ggsave(filename = "../scripts/image_gen_scripts/theta_hat_hist_res_plot.pdf",
+       width = 8.27,
+       height = 11.69)
+
+save(theta_hat_hist_res_plot,file = "../scripts/image_gen_scripts/theta_hat_hist_res_plot.rda")
